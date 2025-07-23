8bc48c497f4b44696665793f70cf9f91c1aa1630
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-07-23 10:57:16 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div className="w-full flex items-center justify-center bg-background">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-start justify-center h-[100dvh]">
+      <div className="relative z-10 flex items-center justify-center text-foreground text-6xl font-italianno font-medium">
+        designs by
+      </div>
+      <div className="relative z-0 flex items-end text-foreground text-9xl mt-[-20px] font-bodoni font-light">
+        ABHISHEK
+      </div>
+    </div>
+  );
+};
+
+export default Home;
